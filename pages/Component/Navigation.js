4dc643caa1763ac501211d9fd991a905caa7172e
Shubham V.  Umap <94427228+Shubhamvumap123@users.noreply.github.com>
2022-10-12 12:49:40 +0530

    Update Navigation.js

--- pages/Component/Navigation.js
+++ pages/Component/Navigation.js
@@ -3,6 +3,7 @@ import Link from 'next/link'
 const Navigation = () => {
   return (
     <nav>
+    <div>Hello</div>
       <ul className='flex justify-around px-4 py-2 bg-gray-300'>
         <li className='px-4 py-2 rounded-full font-bold text-white bg-blue-500 hover:bg-blue-700'>
 <Link href='/'>Home</Link>
